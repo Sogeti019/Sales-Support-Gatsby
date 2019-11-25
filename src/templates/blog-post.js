b3967d6e5c24865cd3ff38e067e318c179ac5ee2
landmichael <Michael.Land@us.sogeti.com>
2019-11-25 11:43:06 -0500

    blog changes for master

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import get from 'lodash/get'
 import Img from 'gatsby-image'
 
 import heroStyles from '../components/hero.module.css'
+import blogStyles from './blog-post.module.css'
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -13,19 +14,20 @@ class BlogPostTemplate extends React.Component {
     return (
       <div style={{ background: '#fff' }}>
         <Helmet title={`${post.title} | ${siteTitle}`} />
-        <div className={heroStyles.hero}>
-          <Img className={heroStyles.heroImage} alt={post.title} sizes={post.heroImage.sizes} />
+        <div style={{width: '50%', margin: 'auto'}} className={heroStyles.hero}>
+          <div  style={{marginLeft: '4%', paddingTop: '20px'}} >
+            <h1 className={blogStyles.topTitle}>{post.title}</h1>
+            <p style={{fontSize: '12px', textTransform: 'uppercase', fontWeight: '400'}}>
+              <a style={{fontSize: '12px', fontWeight: '400', marginRight: '20px'}}>Katie Joiner</a>
+              <span style={{color: '#5b6770', fontWeight: '400'}}>{post.publishDate}</span>
+            </p>
+          </div>
+          <Img style={{margin: '70px 0'}} className={heroStyles.heroImage} alt={post.title} sizes={post.heroImage.sizes} />
         </div>
-        <div className="wrapper">
-          <h1 className="section-headline">{post.title}</h1>
-          <p
-            style={{
-              display: 'block',
-            }}
-          >
-            {post.publishDate}
-          </p>
-          <div
+        <div style={{width: '50%', margin: 'auto'}} className="wrapper">
+          <p style={{display: 'block', color: '#004f71', fontSize: '12px', marginBottom: '20px'}}>FOOD</p>
+          <h1 style={{marginBottom: '30px'}} className={blogStyles.topTitle}>{post.title + ' enjoy the party.'}</h1>
+          <div style={{width: '80%',margin: 'auto', lineHeight: "36px"}}
             dangerouslySetInnerHTML={{
               __html: post.body.childMarkdownRemark.html,
             }}
